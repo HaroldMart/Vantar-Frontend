b869a35c49cc1f834456143c147188d70b5a0c7f
Isael Diroche <isaeldiroche00@gmail.com>
2024-06-12 21:53:43 -0400

    Update: New modules and Sidebar added.

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { BiDislike } from "react-icons/bi";
+
+export default function Page() {
+
+    return (
+        <h1>Home page <BiDislike /></h1>
+    )
+}
---
+++ app/settings/(views)/page.tsx
@@ -0,0 +1,8 @@
+import { BiDislike } from "react-icons/bi";
+
+export default function Page() {
+
+    return (
+        <h1>Settings</h1>
+    )
+}
