cd57206d2b894a3d344ccb103260c898c4eae383
Isael Diroche <isaeldiroche00@gmail.com>
2024-07-01 22:55:00 -0400

    Update: Created component for buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
 import { BiDislike } from "react-icons/bi";
+import Link from "next/link";
+import Button from "@/app/components/Button";
+import { FiArrowLeft, FiArrowRight, FiArrowUp } from 'react-icons/fi'; // Importando algunos iconos como ejemplo
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+
+
+import "@/app/global.css";
+
+
+// export function Button() {
+//   return (
+//     <button className="button-example">
+//       Label
+//     </button>
+//   )
+// }
 
 export default function Page() {
   return (
-    <h1>
-      Home page <BiDislike />
-    </h1>
-  );
+    <div className="flex flex-col items-center space-y-4 p-10 w-full">
+      <Button>
+        Label
+      </Button>
+    </div>
+  )
 }
