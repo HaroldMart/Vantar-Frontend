bf44b1821f61bd79864b539eb537793fb2ceef18
Isael Diroche <isaeldiroche00@gmail.com>
2024-07-06 01:03:28 -0400

    Update: Added new input component
    I added a new component in component folder, with .scss styles and some customizable options.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { BiDislike } from "react-icons/bi";
 import Link from "next/link";
 import Button from "@/app/components/UI/button";
+import Input from "@/app/components/UI/input";
 import { FiArrowLeft, FiArrowRight, FiArrowUp } from 'react-icons/fi'; // Importando algunos iconos como ejemplo
 import { FaArrowRightFromBracket } from "react-icons/fa6";
 
@@ -18,8 +19,14 @@ import "@/app/global.css";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center space-y-4 p-10 w-full">
-      <Button>
+    <div className="flex flex-col items-center space-y-4 p-10 w-96">
+      <div className="w-full">
+        <Input label="Username" placeholder="Enter your username" fullWidth={true} />
+        <Input label="Email" placeholder="Enter your email" fullWidth={true} />
+        <Input label="Password" placeholder="Enter your password" fullWidth={true} />
+      </div>
+
+      <Button fullWidth={true}>
         Label
       </Button>
     </div>
